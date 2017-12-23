9963add182ca2a72272ec7349823f6103be24915
James Logue <logueja@gmail.com>
2017-12-23 11:48:36 -0700

    Initial jsx structure and styling, pass correct props to be rendered

--- src/Author.js
+++ src/Author.js
@@ -1,8 +1,17 @@
-// Create an Author component
-<div className='story-author-info'>
-  <img src={props.authImg} className='story-author-image' />
-  <div className='story-author-text'>
-    <h4>{props.authName}</h4>
-    <h4>{props.estTime} read</h4>
-  </div>
-</div>
+import React from 'react';
+import './Author.css';
+
+const Author = (props) =>  {
+
+  return (
+    <div className='story-author-info'>
+      <img src={props.authProps.authImg} className='story-author-image' />
+      <div className='story-author-text'>
+        <h4>{props.authProps.authName}</h4>
+        <h4>{props.authProps.estTime} read</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Author;
