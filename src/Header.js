fe94f70677b3ddadefe3b53c81a97c4bbff3d447
James Logue <logueja@gmail.com>
2017-12-31 10:40:21 -0700

    Add header media query and style

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,11 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div className='Header'>
-      <a>Write a story </a>
+    <div className='header'>
+      <a className="header-story">Write a story </a>
       <h1>Material</h1>
-      <a>Sign in/Sign up</a>
+      <a className="header-sign-in">Sign in/Sign up</a>
+      <a className="nav-toggle" >&#9776;</a>
     </div>
   )
 }
